694e24b5d933713d866eb9a5f91208d755022f49
iwayIrshad <iway.app7@gmail.com>
2025-11-02 01:45:14 +0530

    Implement theme context (dark & light)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,19 @@
+import useTheme from '@/hooks/useTheme';
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
 const TabsLayout = () => {
+    const { colors } = useTheme();
     return (
         <Tabs
-            screenOptions={{ 
-                tabBarActiveTintColor: '#373f0dff',
-                tabBarInactiveTintColor: '#384305ff',
+            screenOptions={{
+                tabBarActiveTintColor: colors.primary,
+                tabBarInactiveTintColor: colors.textMuted,
                 tabBarStyle: { 
-                    backgroundColor: '#f2f3e8ff',
-                    borderTopWidth: .5,
-                    borderTopColor: '#5d6f03ff',
+                    backgroundColor: colors.surface,
+                    borderTopWidth: 1,
+                    borderTopColor: colors.border,
                     height: 90,
                     paddingBottom: 30,
                     paddingTop: 10
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,24 @@
-import { Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: '#e9ead8'
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen123...</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle Dark Mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: '#e9ead8'
+  }
+});
+
